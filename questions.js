9f0154dc480bc4eef5f4ee9dbe5743843d4c8558
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:57:40 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"The teacher ___ yet.",
+        options:["hasn't came", "hasn't come", "haven't come", "haven't came"],
+        answer: 1
+    },
+    {
+     q:"I hope he ___ a mistake.",
+        options:["hasn't made", "haven't made", "hasn't make", "haven't maden"],
+        answer: 0   
+    },
+    {
+        q:"We ___ them for two years.",
+        options:["haven't meet", "hasn't met", "haven't met", "hasn't meet"],
+        answer: 2
+    },
+    {
+        q:"I ___ such delicious food for ages.",
+        options:["hasn't ate", "haven't ate", "hasn't eaten", "haven't eaten"],
+        answer: 3
+    },
+    {
+        q:"He ___ his keys yet.",
+        options:["hasn't find", "haven't found", "hasn't found", "haven't find"],
+        answer: 2
+    },
+    {
+        q:"She ___ the flowers.",
+        options:["hasn't waterd", "hasn't watered", "haven't watered", "haven't waterd"],
+        answer: 1
+    },
+    {
+        q:"They ___ their flat yet.",
+        options:["haven't cleaned", "haven't cleand", "hasn't cleaned", "hasn't cleand"],
+        answer: 0
+    },
+    {
+        q:"Daniel ___ his car yet.",
+        options:["hasn't wash", "haven't wash", "haven't washed", "hasn't washed"],
+        answer: 3
+    },
+    {
+        q:"We ___ her at school.",
+        options:["haven't saw", "haven't seen", "hasn't saw", "hasn't seen"],
+        answer: 1
+    },
+    {
+        q:"Ann ___ her grandparents yet.",
+        options:["hasn't visit", "haven't visited", "hasn't visited", "haven't visit"],
+        answer: 2
+    }
+
+]
